a9781f08b299ff5b0791b9771343577d9eb70789
neilscallywag <64523806+neilscallywag@users.noreply.github.com>
2021-07-24 03:15:49 +0800

    JSON array for messages

---
+++ getmessage.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include("functions.php");
+
+header('Content-Type: application/json');
+
+$sql = "select * from chat ORDER BY mid ASC;";
+			$stmt = $x->prepare($sql);
+   
+    $stmt->execute();
+    
+	$result = $stmt->fetchall(PDO::FETCH_ASSOC);
+	if(isset($_SESSION["uid"])){
+	foreach($result as $key => $value)
+{
+  $result[$key]['sessionid'] = $_SESSION["uid"];
+}
+	} else {
+		foreach($result as $key => $value)
+{
+  $result[$key]['sessionid'] = null;
+}
+	}
+	echo json_encode($result,JSON_PRETTY_PRINT	);
+	
+	?> 
