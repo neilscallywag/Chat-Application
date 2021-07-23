be3ce98c6e096cae67392f6646c789f1aec64ac6
neilscallywag <64523806+neilscallywag@users.noreply.github.com>
2021-07-24 03:17:29 +0800

    Update getmessage.php

--- getmessage.php
+++ getmessage.php
@@ -1,26 +1,25 @@
 <?php
 session_start();
-include("functions.php");
+include ("functions.php");
 
 header('Content-Type: application/json');
 
-$sql = "select * from chat ORDER BY mid ASC;";
-			$stmt = $x->prepare($sql);
-   
-    $stmt->execute();
-    
-	$result = $stmt->fetchall(PDO::FETCH_ASSOC);
-	if(isset($_SESSION["uid"])){
-	foreach($result as $key => $value)
-{
-  $result[$key]['sessionid'] = $_SESSION["uid"];
+$sql  = "select * from chat ORDER BY mid ASC;";
+$stmt = $x->prepare($sql);
+
+$stmt->execute();
+
+$result = $stmt->fetchall(PDO::FETCH_ASSOC);
+if (isset($_SESSION["uid"])) {
+    foreach ($result as $key    => $value) {
+        $result[$key]['sessionid']        = $_SESSION["uid"];
+    }
 }
-	} else {
-		foreach($result as $key => $value)
-{
-  $result[$key]['sessionid'] = null;
+else {
+    foreach ($result as $key    => $value) {
+        $result[$key]['sessionid']        = null;
+    }
 }
-	}
-	echo json_encode($result,JSON_PRETTY_PRINT	);
-	
-	?> 
+echo json_encode($result, JSON_PRETTY_PRINT);
+
+?>
